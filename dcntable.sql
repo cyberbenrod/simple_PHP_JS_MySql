2f77c4aea122bb9f196c98fa9d086eabff8f63d2
Eleido Benjamin Rodriguez Matias <eleido.rodriguez@gmail.com>
2013-08-04 16:49:32 -0400

    Create dcntable.sql

---
+++ dcntable.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `/*databasename*/`.`toolTable` (
+  `id` iNT NOT NULL AUTO_INCREMENT ,
+	`oracle` INT NOT NULL ,
+	`css` INT NOT NULL ,
+	`libx` INT NOT NULL ,
+	`googledrive` INT NOT NULL ,
+	`notepadplus` INT NOT NULL ,
+	`html` INT NOT NULL ,
+	`filezilla` INT NOT NULL ,
+	`javascript` INT NOT NULL ,
+	`php` INT NOT NULL ,
+	`mysql` INT NOT NULL ,
+	PRIMARY KEY (`id`)
+	
+	);	
+	
+	
